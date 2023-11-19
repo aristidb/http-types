2ca23dd3e3a49cd1857aafd442aed4debf959e12
Felix Paulusma <felix.paulusma@gmail.com>
2023-11-19 01:50:30 +0100

    Status: added @since notations and bit of cleanup

--- Network/HTTP/Types/Status.hs
+++ Network/HTTP/Types/Status.hs
@@ -1,4 +1,6 @@
-{-# LANGUAGE OverloadedStrings, DeriveDataTypeable, DeriveGeneric #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Network.HTTP.Types.Status
 ( Status(..)
 , mkStatus
@@ -106,8 +108,8 @@ module Network.HTTP.Types.Status
 ) where
 
 import qualified Data.ByteString as B
-import Data.Typeable (Typeable)
 import Data.Data (Data)
+import Data.Typeable (Typeable)
 import GHC.Generics (Generic)
 
 -- | HTTP Status.
@@ -121,7 +123,15 @@ import GHC.Generics (Generic)
 data Status
     = Status { statusCode :: Int
              , statusMessage :: B.ByteString
-             } deriving (Show, Typeable, Data, Generic)
+             }
+    deriving (
+        Show,
+        Typeable,
+        Data,
+        -- ^ @since 0.12.4
+        Generic
+        -- ^ @since 0.12.4
+        )
 
 instance Eq Status where
     Status { statusCode = a } == Status { statusCode = b } = a == b
@@ -129,6 +139,7 @@ instance Eq Status where
 instance Ord Status where
     compare Status { statusCode = a } Status { statusCode = b } = a `compare` b
 
+-- | @since 0.7.3
 instance Enum Status where
     fromEnum = statusCode
     toEnum 100 = status100
@@ -181,6 +192,7 @@ instance Enum Status where
     toEnum 511 = status511
     toEnum c   = mkStatus c B.empty
 
+-- | @since 0.11
 instance Bounded Status where
   minBound = status100
   maxBound = status511
@@ -190,18 +202,26 @@ mkStatus :: Int -> B.ByteString -> Status
 mkStatus = Status
 
 -- | Continue 100
+--
+-- @since 0.6.6
 status100 :: Status
 status100 = mkStatus 100 "Continue"
 
 -- | Continue 100
+--
+-- @since 0.6.6
 continue100 :: Status
 continue100 = status100
 
 -- | Switching Protocols 101
+--
+-- @since 0.6.6
 status101 :: Status
 status101 = mkStatus 101 "Switching Protocols"
 
 -- | Switching Protocols 101
+--
+-- @since 0.6.6
 switchingProtocols101 :: Status
 switchingProtocols101 = status101
 
@@ -222,42 +242,62 @@ created201 :: Status
 created201 = status201
 
 -- | Accepted 202
+--
+-- @since 0.6.6
 status202 :: Status
 status202 = mkStatus 202 "Accepted"
 
 -- | Accepted 202
+--
+-- @since 0.6.6
 accepted202 :: Status
 accepted202 = status202
 
 -- | Non-Authoritative Information 203
+--
+-- @since 0.6.6
 status203 :: Status
 status203 = mkStatus 203 "Non-Authoritative Information"
 
 -- | Non-Authoritative Information 203
+--
+-- @since 0.6.6
 nonAuthoritative203 :: Status
 nonAuthoritative203 = status203
 
 -- | No Content 204
+--
+-- @since 0.6.6
 status204 :: Status
 status204 = mkStatus 204 "No Content"
 
 -- | No Content 204
+--
+-- @since 0.6.6
 noContent204 :: Status
 noContent204 = status204
 
 -- | Reset Content 205
+--
+-- @since 0.6.6
 status205 :: Status
 status205 = mkStatus 205 "Reset Content"
 
 -- | Reset Content 205
+--
+-- @since 0.6.6
 resetContent205 :: Status
 resetContent205 = status205
 
 -- | Partial Content 206
+--
+-- @since 0.5.1
 status206 :: Status
 status206 = mkStatus 206 "Partial Content"
 
 -- | Partial Content 206
+--
+-- @since 0.5.1
 partialContent206 :: Status
 partialContent206 = status206
 
@@ -294,34 +334,50 @@ seeOther303 :: Status
 seeOther303 = status303
 
 -- | Not Modified 304
+--
+-- @since 0.6.1
 status304 :: Status
 status304 = mkStatus 304 "Not Modified"
 
 -- | Not Modified 304
+--
+-- @since 0.6.1
 notModified304 :: Status
 notModified304 = status304
 
 -- | Use Proxy 305
+--
+-- @since 0.6.6
 status305 :: Status
 status305 = mkStatus 305 "Use Proxy"
 
 -- | Use Proxy 305
+--
+-- @since 0.6.6
 useProxy305 :: Status
 useProxy305 = status305
 
 -- | Temporary Redirect 307
+--
+-- @since 0.6.6
 status307 :: Status
 status307 = mkStatus 307 "Temporary Redirect"
 
 -- | Temporary Redirect 307
+--
+-- @since 0.6.6
 temporaryRedirect307 :: Status
 temporaryRedirect307 = status307
 
 -- | Permanent Redirect 308
+--
+-- @since 0.9
 status308 :: Status
 status308 = mkStatus 308 "Permanent Redirect"
 
 -- | Permanent Redirect 308
+--
+-- @since 0.9
 permanentRedirect308 :: Status
 permanentRedirect308 = status308
 
@@ -342,10 +398,14 @@ unauthorized401 :: Status
 unauthorized401 = status401
 
 -- | Payment Required 402
+--
+-- @since 0.6.6
 status402 :: Status
 status402 = mkStatus 402 "Payment Required"
 
 -- | Payment Required 402
+--
+-- @since 0.6.6
 paymentRequired402 :: Status
 paymentRequired402 = status402
 
@@ -374,156 +434,228 @@ methodNotAllowed405 :: Status
 methodNotAllowed405 = status405
 
 -- | Not Acceptable 406
+--
+-- @since 0.6.6
 status406 :: Status
 status406 = mkStatus 406 "Not Acceptable"
 
 -- | Not Acceptable 406
+--
+-- @since 0.6.6
 notAcceptable406 :: Status
 notAcceptable406 = status406
 
 -- | Proxy Authentication Required 407
+--
+-- @since 0.6.6
 status407 :: Status
 status407 = mkStatus 407 "Proxy Authentication Required"
 
 -- | Proxy Authentication Required 407
+--
+-- @since 0.6.6
 proxyAuthenticationRequired407 :: Status
 proxyAuthenticationRequired407 = status407
 
 -- | Request Timeout 408
+--
+-- @since 0.6.6
 status408 :: Status
 status408 = mkStatus 408 "Request Timeout"
 
 -- | Request Timeout 408
+--
+-- @since 0.6.6
 requestTimeout408 :: Status
 requestTimeout408 = status408
 
 -- | Conflict 409
+--
+-- @since 0.6.6
 status409 :: Status
 status409 = mkStatus 409 "Conflict"
 
 -- | Conflict 409
+--
+-- @since 0.6.6
 conflict409 :: Status
 conflict409 = status409
 
 -- | Gone 410
+--
+-- @since 0.6.6
 status410 :: Status
 status410 = mkStatus 410 "Gone"
 
 -- | Gone 410
+--
+-- @since 0.6.6
 gone410 :: Status
 gone410 = status410
 
 -- | Length Required 411
+--
+-- @since 0.6.6
 status411 :: Status
 status411 = mkStatus 411 "Length Required"
 
 -- | Length Required 411
+--
+-- @since 0.6.6
 lengthRequired411 :: Status
 lengthRequired411 = status411
 
 -- | Precondition Failed 412
+--
+-- @since 0.6.1
 status412 :: Status
 status412 = mkStatus 412 "Precondition Failed"
 
 -- | Precondition Failed 412
+--
+-- @since 0.6.1
 preconditionFailed412 :: Status
 preconditionFailed412 = status412
 
 -- | Request Entity Too Large 413
+--
+-- @since 0.6.6
 status413 :: Status
 status413 = mkStatus 413 "Request Entity Too Large"
 
 -- | Request Entity Too Large 413
+--
+-- @since 0.6.6
 requestEntityTooLarge413 :: Status
 requestEntityTooLarge413 = status413
 
 -- | Request-URI Too Long 414
+--
+-- @since 0.6.6
 status414 :: Status
 status414 = mkStatus 414 "Request-URI Too Long"
 
 -- | Request-URI Too Long 414
+--
+-- @since 0.6.6
 requestURITooLong414 :: Status
 requestURITooLong414 = status414
 
 -- | Unsupported Media Type 415
+--
+-- @since 0.6.6
 status415 :: Status
 status415 = mkStatus 415 "Unsupported Media Type"
 
 -- | Unsupported Media Type 415
+--
+-- @since 0.6.6
 unsupportedMediaType415 :: Status
 unsupportedMediaType415 = status415
 
 -- | Requested Range Not Satisfiable 416
+--
+-- @since 0.6.1
 status416 :: Status
 status416 = mkStatus 416 "Requested Range Not Satisfiable"
 
 -- | Requested Range Not Satisfiable 416
+--
+-- @since 0.6.1
 requestedRangeNotSatisfiable416 :: Status
 requestedRangeNotSatisfiable416 = status416
 
 -- | Expectation Failed 417
+--
+-- @since 0.6.6
 status417 :: Status
 status417 = mkStatus 417 "Expectation Failed"
 
 -- | Expectation Failed 417
+--
+-- @since 0.6.6
 expectationFailed417 :: Status
 expectationFailed417 = status417
 
 -- | I'm a teapot 418
+--
+-- @since 0.6.6
 status418 :: Status
 status418 = mkStatus 418 "I'm a teapot"
 
 -- | I'm a teapot 418
+--
+-- @since 0.6.6
 imATeapot418 :: Status
 imATeapot418 = status418
 
 -- | Unprocessable Entity 422
 -- (<https://tools.ietf.org/html/rfc4918 RFC 4918>)
+--
+-- @since 0.9.1
 status422 :: Status
 status422 = mkStatus 422 "Unprocessable Entity"
 
 -- | Unprocessable Entity 422
 -- (<https://tools.ietf.org/html/rfc4918 RFC 4918>)
+--
+-- @since 0.9.1
 unprocessableEntity422 :: Status
 unprocessableEntity422 = status422
 
 -- | Upgrade Required 426
 -- (<https://tools.ietf.org/html/rfc7231#section-6.5.15>)
+--
+-- @since 0.10
 status426 :: Status
 status426 = mkStatus 426 "Upgrade Required"
 
 -- | Upgrade Required 426
 -- (<https://tools.ietf.org/html/rfc7231#section-6.5.15>)
+--
+-- @since 0.10
 upgradeRequired426 :: Status
 upgradeRequired426 = status426
 
 -- | Precondition Required 428
 -- (<https://tools.ietf.org/html/rfc6585 RFC 6585>)
+--
+-- @since 0.8.5
 status428 :: Status
 status428 = mkStatus 428 "Precondition Required"
 
 -- | Precondition Required 428
 -- (<https://tools.ietf.org/html/rfc6585 RFC 6585>)
+--
+-- @since 0.8.5
 preconditionRequired428 :: Status
 preconditionRequired428 = status428
 
 -- | Too Many Requests 429
 -- (<https://tools.ietf.org/html/rfc6585 RFC 6585>)
+--
+-- @since 0.8.5
 status429 :: Status
 status429 = mkStatus 429 "Too Many Requests"
 
 -- | Too Many Requests 429
 -- (<https://tools.ietf.org/html/rfc6585 RFC 6585>)
+--
+-- @since 0.8.5
 tooManyRequests429 :: Status
 tooManyRequests429 = status429
 
 -- | Request Header Fields Too Large 431
 -- (<https://tools.ietf.org/html/rfc6585 RFC 6585>)
+--
+-- @since 0.8.5
 status431 :: Status
 status431 = mkStatus 431 "Request Header Fields Too Large"
 
 -- | Request Header Fields Too Large 431
 -- (<https://tools.ietf.org/html/rfc6585 RFC 6585>)
+--
+-- @since 0.8.5
 requestHeaderFieldsTooLarge431 :: Status
 requestHeaderFieldsTooLarge431 = status431
 
@@ -536,71 +668,105 @@ internalServerError500 :: Status
 internalServerError500 = status500
 
 -- | Not Implemented 501
+--
+-- @since 0.6.1
 status501 :: Status
 status501 = mkStatus 501 "Not Implemented"
 
 -- | Not Implemented 501
+--
+-- @since 0.6.1
 notImplemented501 :: Status
 notImplemented501 = status501
 
 -- | Bad Gateway 502
+--
+-- @since 0.6.6
 status502 :: Status
 status502 = mkStatus 502 "Bad Gateway"
 
 -- | Bad Gateway 502
+--
+-- @since 0.6.6
 badGateway502 :: Status
 badGateway502 = status502
 
 -- | Service Unavailable 503
+--
+-- @since 0.6.6
 status503 :: Status
 status503 = mkStatus 503 "Service Unavailable"
 
 -- | Service Unavailable 503
+--
+-- @since 0.6.6
 serviceUnavailable503 :: Status
 serviceUnavailable503 = status503
 
 -- | Gateway Timeout 504
+--
+-- @since 0.6.6
 status504 :: Status
 status504 = mkStatus 504 "Gateway Timeout"
 
 -- | Gateway Timeout 504
+--
+-- @since 0.6.6
 gatewayTimeout504 :: Status
 gatewayTimeout504 = status504
 
 -- | HTTP Version Not Supported 505
+--
+-- @since 0.6.6
 status505 :: Status
 status505 = mkStatus 505 "HTTP Version Not Supported"
 
 -- | HTTP Version Not Supported 505
+--
+-- @since 0.6.6
 httpVersionNotSupported505 :: Status
 httpVersionNotSupported505 = status505
 
 -- | Network Authentication Required 511
 -- (<https://tools.ietf.org/html/rfc6585 RFC 6585>)
+--
+-- @since 0.8.5
 status511 :: Status
 status511 = mkStatus 511 "Network Authentication Required"
 
 -- | Network Authentication Required 511
 -- (<https://tools.ietf.org/html/rfc6585 RFC 6585>)
+--
+-- @since 0.8.5
 networkAuthenticationRequired511 :: Status
 networkAuthenticationRequired511 = status511
 
 -- | Informational class
+--
+-- @since 0.8.0
 statusIsInformational :: Status -> Bool
 statusIsInformational (Status {statusCode=code}) = code >= 100 && code < 200
 
 -- | Successful class
+--
+-- @since 0.8.0
 statusIsSuccessful :: Status -> Bool
 statusIsSuccessful (Status {statusCode=code}) = code >= 200 && code < 300
 
 -- | Redirection class
+--
+-- @since 0.8.0
 statusIsRedirection :: Status -> Bool
 statusIsRedirection (Status {statusCode=code}) = code >= 300 && code < 400
 
 -- | Client Error class
+--
+-- @since 0.8.0
 statusIsClientError :: Status -> Bool
 statusIsClientError (Status {statusCode=code}) = code >= 400 && code < 500
 
 -- | Server Error class
+--
+-- @since 0.8.0
 statusIsServerError :: Status -> Bool
 statusIsServerError (Status {statusCode=code}) = code >= 500 && code < 600
