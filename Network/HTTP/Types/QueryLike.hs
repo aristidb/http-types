bbef7501d43cb742af843dd2d90ae6992e073193
Felix Paulusma <felix.paulusma@gmail.com>
2023-11-19 15:49:03 +0100

    QueryLike: formatted and made imports more explicit

--- Network/HTTP/Types/QueryLike.hs
+++ Network/HTTP/Types/QueryLike.hs
@@ -1,43 +1,44 @@
 {-# LANGUAGE FlexibleInstances #-}
-module Network.HTTP.Types.QueryLike
-(
-  QueryLike(..)
-, QueryKeyLike(..)
-, QueryValueLike(..)
+
+module Network.HTTP.Types.QueryLike (
+    QueryLike (..),
+    QueryKeyLike (..),
+    QueryValueLike (..),
 )
 where
 
-import           Network.HTTP.Types.URI
-import           Data.Maybe
-import qualified Data.ByteString        as B
-import qualified Data.ByteString.Lazy   as L
-import qualified Data.Text              as T
-import qualified Data.Text.Encoding     as T
-import           Control.Arrow
+import Control.Arrow ((***))
+import Data.ByteString as B (ByteString, concat)
+import Data.ByteString.Lazy as L (ByteString, toChunks)
+import Data.Maybe (catMaybes)
+import Data.Text as T (Text, pack)
+import Data.Text.Encoding as T (encodeUtf8)
+
+import Network.HTTP.Types.URI (Query)
 
 -- | Types which can, and commonly are, converted to 'Query' are in this class.
--- 
--- You can use lists of simple key value pairs, with 'B.ByteString' (strict, or lazy: 
+--
+-- You can use lists of simple key value pairs, with 'B.ByteString' (strict, or lazy:
 -- 'L.ByteString'), 'T.Text', or 'String' as the key/value types. You can also have the value
 -- type lifted into a Maybe to support keys without values; and finally it is possible to put
 -- each pair into a Maybe for key-value pairs that aren't always present.
 class QueryLike a where
-  -- | Convert to 'Query'.
-  toQuery :: a -> Query
+    -- | Convert to 'Query'.
+    toQuery :: a -> Query
 
 -- | Types which, in a Query-like key-value list, are used in the Key position.
 class QueryKeyLike a where
-  toQueryKey :: a -> B.ByteString
+    toQueryKey :: a -> B.ByteString
 
 -- | Types which, in a Query-like key-value list, are used in the Value position.
 class QueryValueLike a where
-  toQueryValue :: a -> Maybe B.ByteString
+    toQueryValue :: a -> Maybe B.ByteString
 
 instance (QueryKeyLike k, QueryValueLike v) => QueryLike [(k, v)] where
-  toQuery = map (toQueryKey *** toQueryValue)
+    toQuery = map (toQueryKey *** toQueryValue)
 
 instance (QueryKeyLike k, QueryValueLike v) => QueryLike [Maybe (k, v)] where
-  toQuery = toQuery . catMaybes
+    toQuery = toQuery . catMaybes
 
 instance QueryKeyLike B.ByteString where toQueryKey = id
 instance QueryKeyLike L.ByteString where toQueryKey = B.concat . L.toChunks
@@ -50,4 +51,4 @@ instance QueryValueLike T.Text where toQueryValue = Just . T.encodeUtf8
 instance QueryValueLike [Char] where toQueryValue = Just . T.encodeUtf8 . T.pack
 
 instance QueryValueLike a => QueryValueLike (Maybe a) where
-  toQueryValue = maybe Nothing toQueryValue
+    toQueryValue = maybe Nothing toQueryValue
