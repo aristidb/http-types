4c38d75273355c43261a124f7807d0330b6bc6bc
Felix Paulusma <felix.paulusma@gmail.com>
2023-11-20 01:23:16 +0100

    QueryLike: added @since notations

--- Network/HTTP/Types/QueryLike.hs
+++ Network/HTTP/Types/QueryLike.hs
@@ -22,6 +22,8 @@ import Network.HTTP.Types.URI (Query)
 -- 'L.ByteString'), 'T.Text', or 'String' as the key/value types. You can also have the value
 -- type lifted into a Maybe to support keys without values; and finally it is possible to put
 -- each pair into a Maybe for key-value pairs that aren't always present.
+--
+-- @since 0.7.0
 class QueryLike a where
     -- | Convert to 'Query'.
     toQuery :: a -> Query
@@ -34,21 +36,38 @@ class QueryKeyLike a where
 class QueryValueLike a where
     toQueryValue :: a -> Maybe B.ByteString
 
+-- | @since 0.7.0
 instance (QueryKeyLike k, QueryValueLike v) => QueryLike [(k, v)] where
     toQuery = map (toQueryKey *** toQueryValue)
 
+-- | @since 0.7.0
 instance (QueryKeyLike k, QueryValueLike v) => QueryLike [Maybe (k, v)] where
     toQuery = toQuery . catMaybes
 
+-- | @since 0.7.0
 instance QueryKeyLike B.ByteString where toQueryKey = id
+
+-- | @since 0.7.0
 instance QueryKeyLike L.ByteString where toQueryKey = B.concat . L.toChunks
+
+-- | @since 0.7.0
 instance QueryKeyLike T.Text where toQueryKey = T.encodeUtf8
+
+-- | @since 0.7.0
 instance QueryKeyLike [Char] where toQueryKey = T.encodeUtf8 . T.pack
 
+-- | @since 0.7.0
 instance QueryValueLike B.ByteString where toQueryValue = Just
+
+-- | @since 0.7.0
 instance QueryValueLike L.ByteString where toQueryValue = Just . B.concat . L.toChunks
+
+-- | @since 0.7.0
 instance QueryValueLike T.Text where toQueryValue = Just . T.encodeUtf8
+
+-- | @since 0.7.0
 instance QueryValueLike [Char] where toQueryValue = Just . T.encodeUtf8 . T.pack
 
+-- | @since 0.7.0
 instance QueryValueLike a => QueryValueLike (Maybe a) where
     toQueryValue = maybe Nothing toQueryValue
