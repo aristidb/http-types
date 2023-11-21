ef26a2cdb7959c7e94edf6b05a3863abbe87a52f
Felix Paulusma <felix.paulusma@gmail.com>
2023-11-21 23:44:20 +0100

    formatting everything with fourmolu

--- Network/HTTP/Types/Method.hs
+++ Network/HTTP/Types/Method.hs
@@ -1,45 +1,46 @@
-{-# LANGUAGE DeriveDataTypeable, DeriveGeneric #-}
-module Network.HTTP.Types.Method
-(
-  Method
-, methodGet
-, methodPost
-, methodHead
-, methodPut
-, methodDelete
-, methodTrace
-, methodConnect
-, methodOptions
-, methodPatch
-, StdMethod(..)
-, parseMethod
-, renderMethod
-, renderStdMethod
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveGeneric #-}
+
+module Network.HTTP.Types.Method (
+    Method,
+    methodGet,
+    methodPost,
+    methodHead,
+    methodPut,
+    methodDelete,
+    methodTrace,
+    methodConnect,
+    methodOptions,
+    methodPatch,
+    StdMethod (..),
+    parseMethod,
+    renderMethod,
+    renderStdMethod,
 )
 where
 
-import           Control.Arrow         ((|||))
-import           Data.Array
-import qualified Data.ByteString       as B
+import Control.Arrow ((|||))
+import Data.Array
+import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as B8
-import           Data.Typeable        (Typeable)
-import           Data.Data            (Data)
-import           GHC.Generics         (Generic)
+import Data.Data (Data)
+import Data.Typeable (Typeable)
+import GHC.Generics (Generic)
 
 -- | HTTP method (flat string type).
 type Method = B.ByteString
 
 -- | HTTP Method constants.
 methodGet, methodPost, methodHead, methodPut, methodDelete, methodTrace, methodConnect, methodOptions, methodPatch :: Method
-methodGet     = renderStdMethod GET
-methodPost    = renderStdMethod POST
-methodHead    = renderStdMethod HEAD
-methodPut     = renderStdMethod PUT
-methodDelete  = renderStdMethod DELETE
-methodTrace   = renderStdMethod TRACE
+methodGet = renderStdMethod GET
+methodPost = renderStdMethod POST
+methodHead = renderStdMethod HEAD
+methodPut = renderStdMethod PUT
+methodDelete = renderStdMethod DELETE
+methodTrace = renderStdMethod TRACE
 methodConnect = renderStdMethod CONNECT
 methodOptions = renderStdMethod OPTIONS
-methodPatch   = renderStdMethod PATCH
+methodPatch = renderStdMethod PATCH
 
 -- | HTTP standard method (as defined by RFC 2616, and PATCH which is defined
 --   by RFC 5789).
@@ -54,6 +55,7 @@ data StdMethod
     | OPTIONS
     | PATCH
     deriving (Read, Show, Eq, Ord, Enum, Bounded, Ix, Typeable, Data, Generic)
+
 -- These are ordered by suspected frequency. More popular methods should go first.
 -- The reason is that methodList is used with lookup.
 -- lookup is probably faster for these few cases than setting up an elaborate data structure.
